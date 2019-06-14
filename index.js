d0deeb34a1169234961c31c6e9cc1803f03e6c23
Sachin Mahajan <sachinm78@gmail.com>
2019-06-14 19:21:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,26 +2,14 @@
 let store = { neighborhoods: [], meals: [], customers: [], deliveries: [] }
 
 const Neighborhood = (() => {
-  let neighborhoodIds = 1;
+  let neighborhoodIds = 1
   return class {
     constructor(name) {
-      this.id = neighborhoodIds++;
-      this.name = name;
-      store.neighborhoods.push(this);
+      this.id = neighborhoodIds++
+      this.name = name
+      store.neighborhoods.push(this)
     }
 
-    customers() {
-      return store.customers.filter(customer => customer.neighborhoodId === this.id);
-    }
 
-    meals() {
-      const allMeals = this.customers().map(customer => customer.meals());
-      const merged = [].concat.apply([], allMeals);
-      return [...new Set(merged)];
-    }
-
-    deliveries() {
-      return store.deliveries.filter(delivery => delivery.neighborhoodId === this.id);
-    }
-  };
+  }
 })()
